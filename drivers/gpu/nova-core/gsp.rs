ae3af07c81bfd8f1c3caad2d5e5f83fd49c56661
Alistair Popple <apopple@nvidia.com>
2025-06-26 12:55:36 +1000

    nova-core: gsp: Add GSP command queue handling
    This commit introduces core infrastructure for handling GSP command and
    message queues in the nova-core driver. The command queue system enables
    bidirectional communication between the host driver and GSP firmware
    through a remote message passing interface.
    
    The interface is based on passing serialised data structures over a ring
    buffer with separate transmit and receive queues. Commands are sent by
    writing to the CPU transmit queue and waiting for completion via the
    receive queue.
    
    To ensure safety mutable or immutable (depending on whether it is a send
    or recieve operation) references are taken on the command queue when
    allocating the message to write/read to. This ensures message memory
    remains valid and the command queue can't be mutated whilst an operation
    is in progress.
    
    Currently this is only used by the probe() routine and therefore can
    only used by a single thread of execution. Locking to enable safe access
    from multiple threads will be introduced in a future series when that
    becomes neccessary.
    
    Signed-off-by: Alistair Popple <apopple@nvidia.com>

--- drivers/gpu/nova-core/gsp.rs
+++ drivers/gpu/nova-core/gsp.rs
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: GPL-2.0
+
+use kernel::alloc::flags::GFP_KERNEL;
 use kernel::bindings;
 use kernel::device;
 use kernel::dma::CoherentAllocation;
 use kernel::dma_write;
 use kernel::pci;
 use kernel::prelude::*;
-use kernel::ptr::Alignment;
+use kernel::ptr::{Alignable, Alignment};
+use kernel::sizes::SZ_128K;
 use kernel::transmute::{AsBytes, FromBytes};
 
 use crate::fb::FbLayout;
 use crate::firmware::Firmware;
+use crate::gsp::cmdq::GspCmdq;
 use crate::nvfw::r570_144 as fw;
+
+pub(crate) mod cmdq;
 
 pub(crate) const GSP_PAGE_SHIFT: usize = 12;
 pub(crate) const GSP_PAGE_SIZE: usize = 1 << GSP_PAGE_SHIFT;
@@ -37,6 +43,7 @@ pub(crate) struct GspMemObjects {
     pub logintr: CoherentAllocation<u8>,
     pub logrm: CoherentAllocation<u8>,
     pub wpr_meta: CoherentAllocation<fw::GspFwWprMeta>,
+    pub cmdq: GspCmdq,
 }
 
 pub(crate) fn build_wpr_meta(
@@ -73,20 +80,15 @@ pub(crate) fn build_wpr_meta(
             bootBinOffset: fb_layout.boot.start,
             frtsOffset: fb_layout.frts.start,
             frtsSize: fb_layout.frts.end - fb_layout.frts.start,
-            gspFwWprEnd: fb_layout.vga_workspace.start & !(0x20000 - 1),
+            gspFwWprEnd: fb_layout
+                .vga_workspace
+                .start
+                .align_down(Alignment::new(SZ_128K)),
             gspFwHeapVfPartitionCount: fb_layout.vf_partition_count,
             fbSize: fb_layout.fb.end - fb_layout.fb.start,
             vgaWorkspaceOffset: fb_layout.vga_workspace.start,
             vgaWorkspaceSize: fb_layout.vga_workspace.end - fb_layout.vga_workspace.start,
             bootCount: 0,
-            __bindgen_anon_2: fw::GspFwWprMeta__bindgen_ty_2 {
-                __bindgen_anon_1: fw::GspFwWprMeta__bindgen_ty_2__bindgen_ty_1 {
-                    partitionRpcAddr: 0,
-                    partitionRpcRequestOffset: 0,
-                    partitionRpcReplyOffset: 0,
-                    ..Default::default()
-                },
-            },
             verified: 0,
             ..Default::default()
         }
@@ -107,7 +109,7 @@ fn id8(name: &str) -> u64 {
 }
 
 /// Creates a self-mapping page table for `obj` at its beginning.
-fn create_pte_array(obj: &mut CoherentAllocation<u8>) {
+fn create_pte_array<T: AsBytes + FromBytes>(obj: &mut CoherentAllocation<T>, skip: usize) {
     let num_pages = obj.size().div_ceil(GSP_PAGE_SIZE);
     let handle = obj.dma_handle();
 
@@ -119,7 +121,7 @@ fn create_pte_array(obj: &mut CoherentAllocation<u8>) {
     //  - The allocation size is at least as long as 8 * num_pages as
     //    GSP_PAGE_SIZE is larger than 8 bytes.
     let ptes = unsafe {
-        let ptr = obj.start_ptr_mut().cast::<u64>().add(1);
+        let ptr = obj.start_ptr_mut().cast::<u64>().add(skip);
         core::slice::from_raw_parts_mut(ptr, num_pages)
     };
 
@@ -166,13 +168,15 @@ impl GspMemObjects {
             GFP_KERNEL | __GFP_ZERO,
         )?;
         let mut loginit = create_coherent_dma_object::<u8>(dev, "LOGINIT", 0x10000, &mut libos, 0)?;
-        create_pte_array(&mut loginit);
+        create_pte_array(&mut loginit, 1);
         let mut logintr = create_coherent_dma_object::<u8>(dev, "LOGINTR", 0x10000, &mut libos, 1)?;
-        create_pte_array(&mut logintr);
+        create_pte_array(&mut logintr, 1);
         let mut logrm = create_coherent_dma_object::<u8>(dev, "LOGRM", 0x10000, &mut libos, 2)?;
-        create_pte_array(&mut logrm);
-
+        create_pte_array(&mut logrm, 1);
         let wpr_meta = build_wpr_meta(dev, fw, fb_layout)?;
+
+        // Creates its own PTE array
+        let cmdq = GspCmdq::new(dev)?;
 
         Ok(GspMemObjects {
             libos,
@@ -180,6 +184,7 @@ impl GspMemObjects {
             logintr,
             logrm,
             wpr_meta,
+            cmdq,
         })
     }
 
