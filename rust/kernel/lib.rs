7626a51886e3e215e13abbc2925fba9da4551488
Alexandre Courbot <acourbot@nvidia.com>
2025-08-04 19:47:38 +0900

    rust: add `Alignment` type
    Alignment operations are very common in the kernel. Since they are
    always performed using a power-of-two value, enforcing this invariant
    through a dedicated type leads to fewer bugs and can improve the
    generated code.
    
    Introduce the `Alignment` type, inspired by the nightly Rust type of the
    same name and providing the same interface, and a new `Alignable` trait
    allowing unsigned integers to be aligned up or down.
    
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Danilo Krummrich <dakr@kernel.org>
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -17,6 +17,7 @@
 // the unstable features in use.
 //
 // Stable since Rust 1.79.0.
+#![feature(generic_nonzero)]
 #![feature(inline_const)]
 #![feature(pointer_is_aligned)]
 //
@@ -114,6 +115,7 @@ pub mod platform;
 pub mod prelude;
 pub mod print;
 pub mod processor;
+pub mod ptr;
 pub mod rbtree;
 pub mod regulator;
 pub mod revocable;
