4fc9fb27c860634392aa86bdd14e44757a04e0f1
Timur Tabi <ttabi@nvidia.com>
2025-08-07 14:04:44 -0500

    nova-core: move is_riscv_active into the HAL
    The register used to check with the RISC-V core is active on a
    Falcon differs is not the same across all GPU architectures, so move
    this code in the Falcon HAL.
    
    Signed-off-by: Timur Tabi <ttabi@nvidia.com>

--- drivers/gpu/nova-core/falcon.rs
+++ drivers/gpu/nova-core/falcon.rs
@@ -625,8 +625,7 @@ impl<E: FalconEngine + 'static> Falcon<E> {
     ///
     /// Returns `true` if the RISC-V core is active, `false` otherwise.
     pub(crate) fn is_riscv_active(&self, bar: &Bar0) -> bool {
-        let cpuctl = regs::NV_PRISCV_RISCV_CPUCTL::read(bar, &E::ID);
-        cpuctl.active_stat()
+        self.hal.is_riscv_active(bar)
     }
 
     /// Write the application version to the OS register.
