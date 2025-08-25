3d97dd45023158e550bb79e96e1fa5530fa82088
Alexandre Courbot <acourbot@nvidia.com>
2025-08-26 08:17:37 +0900

    gpu: nova-core: compute layout of more framebuffer regions required for GSP
    Compute more of the required FB layout information to boot the GSP
    firmware.
    
    This information is dependent on the firmware itself, so first we need
    to import and abstract the required firmware bindings in the `nvfw`
    module.
    
    Then, a new FB HAL method is introduced in `fb::hal` that uses these
    bindings and hardware information to compute the correct layout
    information.
    
    This information is then used in `fb` and the result made visible in
    `FbLayout`.
    
    These 3 things are grouped into the same patch to avoid lots of unused
    warnings that would be tedious to work around. As they happen in
    different files, they should not be too difficult to track separately.
    
    Acked-by: Danilo Krummrich <dakr@kernel.org>
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>

--- drivers/gpu/nova-core/gsp.rs
+++ drivers/gpu/nova-core/gsp.rs
@@ -4,9 +4,13 @@ mod boot;
 mod fw;
 
 use kernel::prelude::*;
+use kernel::ptr::Alignment;
+
+pub(crate) use fw::{GspFwWprMeta, LibosParams};
 
 pub(crate) const GSP_PAGE_SHIFT: usize = 12;
 pub(crate) const GSP_PAGE_SIZE: usize = 1 << GSP_PAGE_SHIFT;
+pub(crate) const GSP_HEAP_ALIGNMENT: Alignment = Alignment::new::<{ 1 << 20 }>();
 
 /// GSP runtime data.
 ///
