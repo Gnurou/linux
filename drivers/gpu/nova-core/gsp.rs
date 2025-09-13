0f5c46d2d1eac3ee94ec9adcd0e8a5be5bd282b6
Alexandre Courbot <acourbot@nvidia.com>
2025-09-13 23:36:11 +0900

    gsp: fixup imports

--- drivers/gpu/nova-core/gsp.rs
+++ drivers/gpu/nova-core/gsp.rs
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0
 
 mod boot;
+mod fw;
 
 use kernel::prelude::*;
-
-mod fw;
 
 pub(crate) const GSP_PAGE_SHIFT: usize = 12;
 pub(crate) const GSP_PAGE_SIZE: usize = 1 << GSP_PAGE_SHIFT;
