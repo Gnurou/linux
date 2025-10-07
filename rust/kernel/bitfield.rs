77af873471880f4d6df521ac6e8da018d5bbefc1
Alexandre Courbot <acourbot@nvidia.com>
2025-10-07 15:35:15 +0900

    rust: bitfield: simplify condition
    This condition was uselessly convoluted.
    
    Reported-by: Edwin Peer <epeer@nvidia.com>
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>

--- rust/kernel/bitfield.rs
+++ rust/kernel/bitfield.rs
@@ -204,7 +204,7 @@ macro_rules! bitfield {
     ) => {
         ::kernel::bitfield!(
             @leaf_accessor $vis $name $storage, $hi:$lo $field
-            { |f| <$into_type>::from(if f != 0 { true } else { false }) }
+            { |f| <$into_type>::from(f != 0) }
             bool $into_type => $into_type $(, $comment)?;
         );
     };
