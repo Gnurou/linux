d5435f662b8677545a93373b4c4c80d8b4be40c9
Alexandre Courbot <acourbot@nvidia.com>
2025-10-25 22:52:50 +0900

    simplify debugfs root
    Use unsafe code instead of a Mutex, as we know the entry is never
    modified after the driver is loaded.
    
    Dropping the Mutex solves the lifetime issue of the Mutex guard, which
    would otherwise have required the use of `pin_init_scope`.

--- drivers/gpu/nova-core/nova_core.rs
+++ drivers/gpu/nova-core/nova_core.rs
@@ -18,27 +18,20 @@ mod regs;
 mod sbuffer;
 mod vbios;
 
-use core::pin::Pin;
 use kernel::debugfs::Dir;
 
 pub(crate) const MODULE_NAME: &kernel::str::CStr = <LocalModule as kernel::ModuleMetadata>::NAME;
 
-kernel::sync::global_lock! {
-    // FIXME: Cursor came up with this.
-    // SAFETY: I have no idea
-    pub unsafe(uninit) static DEBUGFS_ROOT: Mutex<Option<Dir>> = None;
-}
+static mut DEBUGFS_ROOT: Option<Dir> = None;
 
 kernel::module_pci_driver! {
     type: driver::NovaCore,
     init: || {
         kernel::pr_info!("Nova Core GPU driver initializing...\n");
-        // FIXME: Cursor came up with this.  It doesn't delete the /nova_core/ directory
-        // on module load.
-        // SAFETY: This isn't safe
-        unsafe { DEBUGFS_ROOT.init() };
         let dir = Dir::new(kernel::c_str!("nova_core"));
-        *DEBUGFS_ROOT.lock() = Some(dir);
+        // SAFETY: we are the only driver code running, so there cannot be any concurrent access to
+        // `DEBUGFS_ROOT`.
+        unsafe { DEBUGFS_ROOT = Some(dir) };
     },
     name: "NovaCore",
     authors: ["Danilo Krummrich"],
